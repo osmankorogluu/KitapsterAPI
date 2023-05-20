cfdb55522adb65b21119e217a9c97156960d8998
Osman <korogluosman85@gmail.com>
2023-05-20 18:22:25 +0300

    Service Registration

---
+++ src/Infrastructure/KitapsterAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using KitapsterAPI.Application.Abstractions;
+using KitapsterAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapsterAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IBookService,BookService>();
+            
+        }
+    }
+}
---
+++ src/Presentation/KitapsterAPI.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using KitapsterAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitapsterAPI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var books = _bookService.GetBooks();
+            return Ok(books);
+        }
+    }
+}
