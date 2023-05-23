e0b2f2938721e66d735bb73da9936f479f6ec6e0
Osman <korogluosman85@gmail.com>
2023-05-23 22:56:40 +0300

    Generic Repository Design Pattern

---
+++ src/Core/KitapsterAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapsterAPI.Application.Repositories
+{
+    public interface IWriteRepository<T>: IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> Remove(T model);
+        Task<bool> Remove(string id);
+    }
+}
