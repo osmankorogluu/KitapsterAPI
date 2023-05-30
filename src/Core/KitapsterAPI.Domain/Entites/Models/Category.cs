d0dea513093832e01dde4aad2f3b4b830789deeb
Osman <korogluosman85@gmail.com>
2023-05-30 23:49:34 +0300

    Category

---
+++ src/Core/KitapsterAPI.Domain/Entites/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using KitapsterAPI.Domain.Entites.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapsterAPI.Domain.Entites.Models
+{
+    public class Category : BaseEntity
+    {
+        public string CategoryName { get; set; }
+    }
+}
